2af515de3bc051acaf4bd7daba7e2137a2aaf8bf
ecshenryho <ecshenryho@gmail.com>
2018-08-14 20:03:14 -0700

    peek form file and modify it

---
+++ peekModifyFile.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cctype>
+using namespace std;
+
+int main() {
+	string fileName;
+	fstream inFile, outFile;
+	char ch;
+	int number;
+
+	cout << "Enter the file name: ";
+	cin >> fileName;
+
+	//open file for input
+	inFile.open(fileName.c_str(), ios::in);
+	if (inFile.fail())
+	{
+		cout << "could not open input file.\n";
+		return 1;
+	}
+
+	//open file for output
+	outFile.open("modified.txt", ios::out);
+	if (outFile.fail())
+	{
+		cout << "Could not open the output file.\n";
+		return 2;
+	}
+
+	ch = inFile.peek();
+	while (ch != EOF)
+	{
+		cout << "Inside while loop\n";
+		//if it is a digit, increment by 1
+		if (isdigit(ch))
+		{
+			inFile >> number;
+			outFile << number + 1;
+		}
+		//else just read and write to the file
+		else
+		{
+			ch = inFile.get();
+			outFile << ch;
+		}
+
+		ch = inFile.peek();
+	}
+
+	inFile.close();
+	outFile.close();
+	return 0;
+}

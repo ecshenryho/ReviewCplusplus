c633b9827fa53eb9bd8a4247c790898c6dde4635
ecshenryho <ecshenryho@gmail.com>
2018-08-15 00:08:42 -0700

    add function with more than 1 template parameters

--- functionTemplate.cpp
+++ functionTemplate.cpp
@@ -7,10 +7,17 @@ T sum(T a, T b) {
 	return (a + b);
 }
 
+//Multiple template parameters
+template <class T, class U>
+bool are_equal (T a, U b)
+{
+  return (a==b);
+}
 
 int main() {
 	cout << "Result with integer values pass: " << sum<int>(4, 6) << endl;
 	cout << "Result with double values pass: " << sum<double>(4.5, 5.5) << endl;
+    cout<<are_equal(5, 10.5)<<endl;
 	system("pause");
 	return 0;
 }
